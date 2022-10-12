a9774458cd080b6b0c8ef476064464ab4e78307e
EbbaSchmid <justebba@gmail.com>
2022-10-12 15:56:20 -0500

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route>
+        
+      </Route>
+
+
+
+    </Routes>
+    </>
+  )
+}
+
+
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <header className="App-header">
+
+      {/* <Link to="/class-list">Class List</Link> */}
+
+    
+    </header>
+  )
+}
+
+export default NavBar
